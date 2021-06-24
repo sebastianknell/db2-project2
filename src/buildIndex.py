d8ba1d3632b4db8d98fa2c4481673629ae5104ab
Sebastian Knell <sebastianknell@hotmail.com>
2021-06-24 12:45:05 -0500

    write chunks in different files

--- src/buildIndex.py
+++ src/buildIndex.py
@@ -3,30 +3,50 @@ import pandas as pd
 import numpy as np
 from nltk import SnowballStemmer, word_tokenize
 from nltk.corpus import stopwords
+from ast import literal_eval
 
 CHUNK_SIZE = 1000
 DATA_FILE = "src/data/articles1.csv"
-TEMP_FILE = "src/data/temp.txt"
+TEMP_FILE = "src/data/temp{}.txt"
 INDEX_FILE = "src/data/index.txt"
 
 # Index structure
 # term:[(docID,tf)] -> df = length of list
 def writeIndex(index, filename):
-    with open(filename, 'a+') as  file:
+    with open(filename, 'a+') as file:
         for term, docDict in index.items():
             line = str(term)
             line += ':['
             for tup in docDict.items():
-                # Remove whitespace between str(tuple)
-                tupStr = str(tup)
-                tupList = tupStr.split()
-                line += str(''.join(tupList))
+                line += str(tup)
+                line += ','
             line += ']'
             line += '\n'
             file.writelines(line)
 
-def mergeByBlocks():
-    return
+
+def readIndex(filename):
+    index = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            # Read term
+            line = line.strip()
+            term = ""
+            i = 0
+            while(i < len(line) and line[i] != ':'):
+                term += line[i]
+                i += 1
+            i += 1
+            # Read list of tuples and add to index
+            tuples = literal_eval(line[i:])
+            index[term] = dict(tuples)
+
+
+def mergeByBlocks(chunkCount):
+    for i in range(0, chunkCount-1, 2):
+        index1 = readIndex(TEMP_FILE.format(i))
+        index2 = readIndex(TEMP_FILE.format(i+1))
+        # Merge
 
 
 def buildIndex():
@@ -34,7 +54,9 @@ def buildIndex():
         stemmer = SnowballStemmer('english')
         stoplist = stopwords.words('english')
         stoplist += ['.', '?', '-', '«', '»', ',', '(', ')', ':', ';']
+        chunkCount = 0
         for chunk in reader:
+            chunkCount += 1
             # Process chunk
             localIndex = {}
             for id, row in chunk.iterrows():
@@ -48,7 +70,7 @@ def buildIndex():
                 # 3. Stemming
                 for i in range(len(words)):
                     words[i] = stemmer.stem(words[i])
-                
+
                 # 4. Build index
                 for token in words:
                     if token in localIndex.keys():
@@ -57,9 +79,11 @@ def buildIndex():
                         else:
                             localIndex[token][id] = 1
                     else:
-                        localIndex[token] = {id : 1}
-                    
+                        localIndex[token] = {id: 1}
+
             # Write chunck
-            writeIndex(localIndex, TEMP_FILE)
+            writeIndex(localIndex, TEMP_FILE.format(chunkCount))
+            break
+
 
 buildIndex()
