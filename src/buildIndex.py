865f181ea70b079234eba7261da67d45f7803ea3
Sebastian Knell <sebastianknell@hotmail.com>
2021-06-25 20:08:21 -0500

    write index all at once. see dev for BSB approach

--- src/buildIndex.py
+++ src/buildIndex.py
@@ -1,12 +1,9 @@
 import pandas as pd
-import numpy as np
 from nltk import SnowballStemmer, word_tokenize
 from nltk.corpus import stopwords
 from ast import literal_eval
 
-CHUNK_SIZE = 2500
 DATA_FILE = "src/data/articles1.csv"
-TEMP_FILE = "src/data/temp/chunk{}.txt"
 INDEX_FILE = "src/data/index.txt"
 
 # Index structure
@@ -39,54 +36,42 @@ def readIndex(filename):
             index[l[0]] = dict(tuples)
     return index
 
-def mergeByChunks(chunkCount):
-    for i in range(0, chunkCount-1, 2):
-        index1 = readIndex(TEMP_FILE.format(i))
-        index2 = readIndex(TEMP_FILE.format(i+1))
-        # Merge
-
 
 def buildIndex():
-    with pd.read_csv(DATA_FILE, chunksize=CHUNK_SIZE, encoding='UTF-8') as reader:
-        stemmer = SnowballStemmer('english')
-        stoplist = stopwords.words('english')
-        stoplist += ['.', ',', '?', '-', '–', '«', '»', '(', ')', ':', ';', '#', '!', '$', '@', '%', '^', '*', '&', '*', '+', '']
-        chunkCount = 0
-        for chunk in reader:
-            chunkCount += 1
-            # Process chunk
-            localIndex = {}
-            for id, row in chunk.iterrows():
-                text = row['title'] + ' ' + row['content']
-                # 1. Tokenize
-                words = word_tokenize(text.lower().strip())
-                # 2. Filter stopswords
-                i = 0
-                while i < len(words):
-                    if words[i] in stoplist:
-                        words.pop(i)
-                    else:
-                        i += 1
-                # 3. Stemming
-                for i in range(len(words)):
-                    words[i] = stemmer.stem(words[i])
+    file = pd.read_csv(DATA_FILE, encoding='UTF-8')
+    stemmer = SnowballStemmer('english')
+    stoplist = stopwords.words('english')
+    stoplist += ['.', ',', '?', '-', '–', '«', '»',
+                  '(', ')', ':', ';', '#', '!', '$', '@', '%', '^', '*', '&', '*', '+', '']
+    index = {}
+    for id, row in file.iterrows():
+        text = row['title'] + ' ' + row['content']
+        # 1. Tokenize
+        words = word_tokenize(text.lower().strip())
+        # 2. Filter stopswords
+        i = 0
+        while i < len(words):
+            if words[i] in stoplist:
+                words.pop(i)
+            else:
+                i += 1
+        # 3. Stemming
+        for i in range(len(words)):
+            words[i] = stemmer.stem(words[i])
 
-                # 4. Build index
-                for token in words:
-                    if token in localIndex.keys():
-                        if id in localIndex[token].keys():
-                            localIndex[token][id] += 1
-                        else:
-                            localIndex[token][id] = 1
-                    else:
-                        localIndex[token] = {id: 1}
+        # 4. Build index
+        for token in words:
+            if len(token) > 0 and token in index.keys():
+                if id in index[token].keys():
+                    index[token][id] += 1
+                else:
+                    index[token][id] = 1
+            else:
+                index[token] = {id: 1}
 
-            # Write chunck
-            localIndex = dict(sorted(localIndex.items(), key = lambda elem : elem[0]))
-            writeIndex(localIndex, TEMP_FILE.format(chunkCount))
-            break
-        # mergeByChunks(chunkCount)
+    index = dict(sorted(index.items(), key=lambda elem: elem[0]))
+    writeIndex(index, INDEX_FILE)
+
 
 # buildIndex()
-index = readIndex(TEMP_FILE.format(1))
-print(index['tweet'])
+readIndex(INDEX_FILE)
