a384fbb14ef3f4cfcb2f97f417d460f36a94f122
Sebastian Knell <sebastianknell@hotmail.com>
2021-06-27 23:51:22 -0500

    query model

--- src/app.py
+++ src/app.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, Response
+from flask import request
 from flask_cors import CORS
+import json
+
+from entities import New
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -8,8 +12,11 @@ cors = CORS(app)
 def index():
     return "<p>Hello, World!</p>"
 
-@app.route('/search/<query>', methods=['GET'])
-def search(query):
-    print("Processing query")
-    print(query)
-    return "Query processed"
+
+@app.route('/search', methods=['POST'])
+def search():
+    msg = json.loads(request.data)
+    print("Query {} recieved".format(msg['query']))
+    new = New(1, 'Some title', 'New York Times', 2021, 'Some content')
+    s = json.dumps(vars(new))
+    return Response(s, 200, mimetype='application/json')
